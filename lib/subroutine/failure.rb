6f1524f6026ee8bca9c9b6e9cd2df22a1ad3567a
James Cox <james@imaj.es>
2022-08-17 22:21:20 -0500

    ok, lets make this smarter

--- lib/subroutine/failure.rb
+++ lib/subroutine/failure.rb
@@ -3,10 +3,24 @@
 module Subroutine
   class Failure < StandardError
 
-    attr_accessor :errors
+    attr_accessor :record, :errors
 
     def initialize(record)
-      errors = record.respond_to?(:errors) ? record.errors.full_messages.join(', ') : record
+      # a StandardError takes a string, but we want to be able to also keep
+      # track of a more advanced error state
+      # https://api.rubyonrails.org/v5.2/classes/ActiveModel/Errors.html
+      #
+      if record.respond_to?(:errors) && record.errors.is_a?(ActiveModel::Errors)
+        @record = record
+        @errors = @record.errors.full_messages.join(', ')
+      else
+        raise ArgumentError.new "subroutine failure - record is #{record}"
+        # @record = ActiveModel::Errors.new
+
+        # generate a new errors class....
+        #
+      end
+
       super(errors)
     end
   end
